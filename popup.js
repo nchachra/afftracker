7de758247e3b12e329669b94b9b6f90355431810
nayha <nayha@nayha-laptop.(none)>
2014-02-18 17:25:47 -0800

    Works for Amazon and Hostgator.

---
+++ popup.js
@@ -0,0 +1,84 @@
+var afftracker_loader = {
+
+  /**
+   * List of merchants to display.
+   *
+   * @private
+   */
+  merchants: ["amazon", "hostgator"],
+
+  /**
+   * List of specifications to render for each merchant.
+   *
+   * @private
+   */
+  specs: {"aff": "Affiliate:",
+          "time": "Set at: ",
+          "rsc-type": "Rendered as:",
+          "referer": "Referrer:",
+          "site": "Visited: "},
+
+  /**
+   * Pulls the stored values from storage and populates the DOM.
+   *
+   * @public
+   */
+  populateDom: function() {
+    var divEl = document.getElementById("merchant-info");
+    this.merchants.forEach(function(merchant, index) {
+      var tableEl = document.createElement('table');
+      var store_key = "commtrack_" + merchant;
+
+      // Every table's first cell is the icon img.
+      var icon_img = document.createElement('img');
+      icon_img.src = "icons/" + merchant + "_icon.png";
+      var icon_cell = document.createElement('td');
+      icon_cell.setAttribute('rowspan', 5);
+      icon_cell.appendChild(icon_img);
+
+      var icon_set = false;
+
+      for (var key in afftracker_loader.specs) {
+        if (afftracker_loader.specs.hasOwnProperty(key)) {
+          var row = document.createElement('tr');
+          if (!icon_set) {
+            row.appendChild(icon_cell);
+            icon_set = true;
+          }
+          var spec_cell = document.createElement('td');
+          spec_cell.setAttribute("style", "font-weight:bold;");
+          spec_cell.innerHTML = afftracker_loader.specs[key]; //fix this
+
+          var spec_id_cell = document.createElement('td');
+          spec_id_cell.id = merchant + "-" + key;
+          row.appendChild(spec_cell);
+          row.appendChild(spec_id_cell);
+          tableEl.appendChild(row);
+        }
+      };
+      // Change background color for even numbered rows.
+          if (index % 2 === 1) {
+            tableEl.setAttribute("style", "background-color: #E6E6E6;");
+          }
+      divEl.appendChild(tableEl);
+
+      chrome.storage.sync.get(store_key, function(result) {
+        var store_info = result[store_key];
+        if (store_info) {
+          document.getElementById(merchant + "-aff").innerHTML = store_info.aff_id;
+          document.getElementById(merchant + "-time").innerHTML = new Date(store_info.timestamp).toString();
+          document.getElementById(merchant + "-rsc-type").innerHTML = store_info.type;
+          document.getElementById(merchant + "-referer").innerHTML = store_info.referer;
+          document.getElementById(merchant + "-site").innerHTML = store_info.site;
+        }
+      });
+    });
+  }
+};
+
+
+// This should really be done with the event registration API? The alternate
+// to the following.
+document.addEventListener('DOMContentLoaded', function () {
+  afftracker_loader.populateDom();
+});
---
+++ trackrequest.js
@@ -0,0 +1,124 @@
+
+
+var afftracker_setter = {
+  /**
+   * Holds the state for merchant until it is saved in storage.
+   *
+   * @private
+   */
+  commtrack_amazon: {},
+  commtrack_hostgator: {},
+
+  /**
+   * Parses out Amazon affiliate ID.
+   *
+   * @param {string} url Requested Amazon URL.
+   * @return {string} Affiliate's ID.
+   * @private
+   */
+  parse_amazon_aff_id: function(url) {
+    args = url.substring(url.lastIndexOf("/") + 1);
+    if (args.indexOf("&tag=") != -1) {
+      args = args.substring(args.indexOf("&tag=") + 5);
+    } else if (args.indexOf("?tag=") != -1) {
+      args = args.substring(args.indexOf("?tag=") + 5);
+    };
+
+    if (args.indexOf("&") != -1) {
+      args = args.substring(0, args.indexOf("&"));
+    };
+
+    // When tag isn't explicitly specified, it's the entire string after
+    // the last slash as far as I know.
+    return args;
+  },
+
+  /**
+   * Callback for headers received events. Searches for Set-Cookie header
+   * and parses out the affiliate ID.
+   *
+   * @param{Webrequest} details
+   */
+  response_callback: function(details) {
+    if (details.url.indexOf("amazon.com") !== -1) {
+      // Amazon.com's UserPref cookie is an affiliate cookie.
+      details.responseHeaders.forEach(function(header) {
+        if (header.name.toLowerCase() === "set-cookie") {
+          if (header.value.substring(0, 9) === "UserPref=") {
+            // Amazon's affiliate id does not show up in the Cookie.
+            aff_id = afftracker_setter.parse_amazon_aff_id(details.url);
+            afftracker_setter.commtrack_amazon["cookie"] = header.value;
+            afftracker_setter.commtrack_amazon["aff_id"] = aff_id;
+            afftracker_setter.commtrack_amazon["type"] = details.type;
+            afftracker_setter.commtrack_amazon["timestamp"] = details.timeStamp;
+            chrome.storage.sync.set(
+              {'commtrack_amazon': afftracker_setter.commtrack_amazon},
+              function() {});
+            afftracker_setter.commtrack_amazon = null;
+          }
+        }
+      });
+    } else if (details.url.indexOf("hostgator.com") !== -1) {
+      details.responseHeaders.forEach(function(header) {
+        if(header.name.toLowerCase() === "set-cookie") {
+          if (header.value.indexOf("GatorAffiliate") !== -1) {
+            // Split cookie value on semi-colon, take the first break
+            // remove numbernumber. from beginning, keep the rest as aff.
+            var aff_id = header.value.split(";", 1)[0].split(".")[1];
+            afftracker_setter.commtrack_hostgator["cookie"] = header.value;
+            afftracker_setter.commtrack_hostgator["aff_id"] = aff_id;
+            afftracker_setter.commtrack_hostgator["type"] = details.type;
+            afftracker_setter.commtrack_hostgator["timestamp"] = details.timeStamp;
+            chrome.storage.sync.set(
+              {"commtrack_hostgator": afftracker_setter.commtrack_hostgator},
+              function() {});
+            afftracker_setter.commtrack_hostgator = null;
+          }
+        }
+      });
+    }
+  },
+
+
+  /**
+   * Callback for when a request is about to be made. We get the request
+   * headers here and pull out the referer.
+   *
+   * @param{Webrequest} details
+   */
+  request_callback: function(details) {
+    if (details.url.indexOf("http://secure.hostgator.com") === 0) {
+      details.requestHeaders.forEach(function(header) {
+        if (header.name.toLowerCase() === "referer") {
+          afftracker_setter.commtrack_hostgator["referer"] = header.value;
+          chrome.tabs.get(details.tabId, function(tab) {
+            afftracker_setter.commtrack_hostgator["site"] = tab.url;
+          });
+        }
+      });
+    }
+    if (details.url.indexOf("amazon.com") !== -1) {
+      details.requestHeaders.forEach(function(header) {
+        // Ignore amazon redirects to itself.
+        if (header.name.toLowerCase() === "referer" &&
+            header.value.indexOf("amazon.com") === -1) {
+          afftracker_setter.commtrack_amazon["referer"] = header.value;
+          chrome.tabs.get(details.tabId, function(tab) {
+            afftracker_setter.commtrack_amazon["site"] = tab.url;
+          });
+        }
+      });
+    }
+  },
+};
+
+
+/**
+ * Analyze all responses. While the filter allows all URLs here, the manifest
+ * restricts analysis to only merchant URLs.
+ */
+chrome.webRequest.onHeadersReceived.addListener(
+    afftracker_setter.response_callback, {urls: ["<all_urls>"]}, ["responseHeaders"]);
+
+chrome.webRequest.onSendHeaders.addListener(
+    afftracker_setter.request_callback, {urls: ["<all_urls>"]}, ["requestHeaders"]);

4cb117f54c70d7e24697f6c5ad9e0cd0a1e4b01c
Attila Bodis <attila@google.com>
2014-05-25 18:52:33 -0700

    Add Jasmine 2.0 JS unit test framework (no tests yet)

---
+++ test/spec/testspec.js
@@ -0,0 +1,18 @@
+describe("AffiliateTracker", function() {
+
+  beforeEach(function() {
+	// TODO: Implement...
+  });
+
+  it("should be true", function() {
+	// TODO: Replace with real test...
+	expect(true).toBe(true);
+	expect("heart").toEqual("heart");
+  });
+
+  it("should be <3", function() {
+	// TODO: Replace with real test...
+	expect("<3").toEqual("<3");
+  });
+
+});

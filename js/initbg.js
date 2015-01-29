5cdd857d1b5533e85cc7e1634ba23d568e160b69
Neha Chachra <nchachra@fb.com>
2015-01-29 12:43:07 -0800

    Placeholder for a callback that actually lets me determine if the extension was freshly installed, or updated. Also a clearner way to init.

--- js/initbg.js
+++ js/initbg.js
@@ -1,36 +1,43 @@
-/**
-  * Initialize the extension. Specifically, it relies on the userid creation
-  * promise to have a userid. Only when one is created it:
-  * 1) Initializes and parses the existing cookies to display in the popup.
-  * 2) Sends a list of currently installed extensions to the server. We track
-  *    these because extensions like adblocker affect affiliate cookies. This
-  *    excludes some extensions that chrome comes with by default.
-  * 3) Attaches listeners for monitoring cookies while browsing.
-  */
-ATUtils.getUserId().then(function(userId) {
-  console.assert(typeof ATBg.userId === "string",
-    "Generated UserId should be a string.");
-  // Every second, send data to server if there is any.
-  setInterval(ATBg.sendCookiesToServer, 1000);
+ATInit = {
 
-  ATBg.processExistingCookies();
-  ATBg.processExistingExtensions();
+  /**
+    * Initialize the extension. Specifically, it relies on the userid creation
+    * promise to have a userid. Only when one is created it:
+    * 1) Initializes and parses the existing cookies to display in the popup.
+    * 2) Sends a list of currently installed extensions to the server. We track
+    *    these because extensions like adblocker affect affiliate cookies. This
+    *    excludes some extensions that chrome comes with by default.
+    * 3) Attaches listeners for monitoring cookies while browsing.
+    */
+  initialize: function(info) {
+    ATUtils.getUserId().then(function(userId) {
+      console.assert(typeof ATBg.userId === "string",
+        "Generated UserId should be a string.");
+      // Every second, send data to server if there is any.
+      setInterval(ATBg.sendCookiesToServer, 1000);
 
-  // We need this event to be able to highlight DOM element and get its
-  // attributes.
-  chrome.tabs.onUpdated.addListener(ATBg.tabLoadCallback);
+      ATBg.processExistingCookies();
+      ATBg.processExistingExtensions();
 
-  chrome.webRequest.onSendHeaders.addListener(
-    ATBg.requestCallback, {urls: ["<all_urls>"]}, ["requestHeaders"]);
-  // There is an inherent assumption that onSendHeaders is fired before
-  //   onHeadersReceived. It would be odd if this assumption is violated.
-  chrome.webRequest.onHeadersReceived.addListener(
-    ATBg.responseCallback, {urls: ["<all_urls>"]}, ["responseHeaders"]);
+      // We need this event to be able to highlight DOM element and get its
+      // attributes.
+      chrome.tabs.onUpdated.addListener(ATBg.tabLoadCallback);
 
-}, function(error) {
-  //TODO:
-  //Theoretically the extension can still notify user. Change this once
-  //data collection is not needed for the project. Or at least notify user
-  //to try reset the extension. Otherwise it'll quietly sit forever.
-  console.error("Failed to generate userid!", ATBg.userId);
-});
+      chrome.webRequest.onSendHeaders.addListener(
+        ATBg.requestCallback, {urls: ["<all_urls>"]}, ["requestHeaders"]);
+      // There is an inherent assumption that onSendHeaders is fired before
+      //   onHeadersReceived. It would be odd if this assumption is violated.
+      chrome.webRequest.onHeadersReceived.addListener(
+        ATBg.responseCallback, {urls: ["<all_urls>"]}, ["responseHeaders"]);
+
+    }, function(error) {
+      //TODO:
+      //Theoretically the extension can still notify user. Change this once
+      //data collection is not needed for the project. Or at least notify user
+      //to try reset the extension. Otherwise it'll quietly sit forever.
+      console.error("Failed to generate userid!", ATBg.userId);
+    });
+  },
+}
+
+chrome.runtime.onInstalled.addListener(ATInit.initialize);
